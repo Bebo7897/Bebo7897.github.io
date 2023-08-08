ec7b16947318eb35d2be5a7be3613510a2e2f039
Bebo7897 <81861464+Bebo7897@users.noreply.github.com>
2023-08-07 22:08:09 -0400

    updated js file again

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,3 @@
-document.querySelector('video').defaultPlaybackRate = 3.0;
-
 // These functions open and close the contact form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
